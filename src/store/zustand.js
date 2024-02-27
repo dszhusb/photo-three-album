022cb3578a01d6277dbdc3c1ea9515574f961219
dszhusb <71653499+dszhusb@users.noreply.github.com>
2024-02-27 00:04:41 -0500

    Photo Upload

--- src/store/zustand.js
+++ src/store/zustand.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 
 export const useStore = create((set) => ({
     capsule: { url: '/images/placeholder.png', type: 'cube' },
-    urlList: ['/9.JPG', '/10.JPG', '/11.JPG', '/12.JPG', '/13.JPG', '/14.JPG', '/15.JPG', '/16.JPG', "/17.jpg"],
+    urlList: ['/9.JPG', '/10.JPG', '/11.JPG', '/12.JPG', '/13.JPG', '/14.JPG', '/15.JPG', '/16.JPG', "/17.jpg", "/18.jpg", "/19.jpg", "/20.jpg", "/21.jpg", "/22.jpg", "/23.jpg", "/24.jpg", "/25.jpg", "/26.jpg"],
     setCapsule: (cap) => set({ capsule: cap }),
     setUrlList: (list) => set({ urlList: list}),
 }))
